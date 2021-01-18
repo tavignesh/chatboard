9b1af631f191548c1f2182a7982f5f2f68e55ee7
james <james@james.james>
2021-01-18 14:12:44 -0500

    add user friendly config.json load error message

--- settings.py
+++ settings.py
@@ -1,7 +1,10 @@
 import json
-
-with open('config.json', 'r') as config_file:
-    configdata = json.load(config_file)
-    token = configdata["token"]
-    prefix = configdata["prefix"]
-    mongo_url = configdata["mongo_url"]
+try:
+    with open('config.json', 'r') as config_file:
+        configdata = json.load(config_file)
+        token = configdata["token"]
+        prefix = configdata["prefix"]
+        mongo_url = configdata["mongo_url"]
+except:
+    print("Invalid config.json file! Exiting now.")
+    exit()
